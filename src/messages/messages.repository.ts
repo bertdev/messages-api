1e5b7d4e8c79a3fb301f806ac5c7a2b28e24c178
bertdev <herbertmatos10@hotmail.com>
2022-05-19 13:35:47 -0300

    adiciona o repository para messages

---
+++ src/messages/messages.repository.ts
@@ -0,0 +1,27 @@
+import { readFile, writeFile } from 'fs/promises';
+
+export class MessagesRepsitory {
+  async findOne(id: string) {
+    const contents = await readFile('messages.json', 'utf-8');
+    const messages = JSON.parse(contents);
+
+    return messages[id];
+  }
+
+  async findAll() {
+    const contents = await readFile('messages.json', 'utf-8');
+    const messages = JSON.parse(contents);
+
+    return messages;
+  }
+
+  async create(content: string) {
+    const contents = await readFile('messages.json', 'utf-8');
+    const messages = JSON.parse(contents);
+
+    const id = Math.floor(Math.random() * 999);
+    messages[id] = { id, content };
+
+    await writeFile('messages.json', JSON.stringify(messages));
+  }
+}
