c2252bca8ac27a55a569260cbed1ba60072e94cc
bertdev <herbertmatos10@hotmail.com>
2022-05-19 13:01:35 -0300

    cria validacao para o body recebido na criacao de uma message

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { CreateMessageDto } from './dtos/create-message.dto';
 
 @Controller('messages')
 export class MessagesController {
@@ -8,8 +9,12 @@ export class MessagesController {
   }
 
   @Post()
-  createMessages() {}
+  createMessages(@Body() body: CreateMessageDto) {
+    return body;
+  }
 
   @Get('/:id')
-  getMessage() {}
+  getMessage(@Param('id') id: number) {
+    return id;
+  }
 }
